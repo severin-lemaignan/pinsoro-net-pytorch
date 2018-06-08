bcb903e72033cb53dcb240e4f6685f07e26f8287
Séverin Lemaignan <severin.lemaignan@brl.ac.uk>
2018-06-08 17:28:11 +0100

    Changed layer names for PInSoRo

--- model.py
+++ model.py
@@ -2,22 +2,25 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 
-class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super(RNN, self).__init__()
+class PInSoRoRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, device):
+        super(PInSoRoRNN, self).__init__()
+
+        self.device = device
 
         self.hidden_size = hidden_size
 
-        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
-        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.i2h_poses = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o_poses = nn.Linear(input_size + hidden_size, output_size)
         self.softmax = nn.LogSoftmax()
 
     def forward(self, input, hidden):
-        combined = torch.cat((input, hidden), 1)
-        hidden = self.i2h(combined)
-        output = self.i2o(combined)
+        combined_poses = torch.cat((input, hidden), 1)
+        hidden_poses = self.i2h_poses(combined_poses)
+        output_poses = self.i2o_poses(combined_poses)
         output = self.softmax(output)
-        return output, hidden
+        return output, hidden_poses
 
     def initHidden(self):
-        return Variable(torch.zeros(1, self.hidden_size))
+
+        return Variable(torch.zeros(1, self.hidden_size), device=self.device)
