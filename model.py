fa362f18bc136a2e7d905cdedd97beb3a183b3be
Séverin Lemaignan <severin.lemaignan@brl.ac.uk>
2018-06-12 11:42:09 +0100

    model: Minor  fixes

--- model.py
+++ model.py
@@ -3,24 +3,25 @@ import torch.nn as nn
 from torch.autograd import Variable
 
 class PInSoRoRNN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, device):
+    def __init__(self, input_size, hidden_size, output_size):
         super(PInSoRoRNN, self).__init__()
-
-        self.device = device
 
         self.hidden_size = hidden_size
 
         self.i2h_poses = nn.Linear(input_size + hidden_size, hidden_size)
         self.i2o_poses = nn.Linear(input_size + hidden_size, output_size)
-        self.softmax = nn.LogSoftmax()
+        self.softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, input, hidden):
-        combined_poses = torch.cat((input, hidden))
+        #import pdb;pdb.set_trace()
+
+        combined_poses = torch.cat((input, hidden), 1)
         hidden_poses = self.i2h_poses(combined_poses)
         output_poses = self.i2o_poses(combined_poses)
         output = self.softmax(output_poses)
         return output, hidden_poses
 
-    def initHidden(self):
+    @staticmethod
+    def initHidden(batch_size, hidden_size, device):
 
-        return torch.zeros(1, self.hidden_size, device=self.device, requires_grad=True)
+        return torch.zeros(batch_size, hidden_size, device=device, requires_grad=True)
