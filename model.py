9293feaf4d13569df35762446eacf158017a41fb
Séverin Lemaignan <severin.lemaignan@brl.ac.uk>
2018-06-14 09:20:07 +0100

    [model] v0 - simple RNN with 4 FC layers at output

--- model.py
+++ model.py
@@ -9,7 +9,14 @@ class PInSoRoRNN(nn.Module):
         self.hidden_size = hidden_size
 
         self.i2h_poses = nn.Linear(input_size + hidden_size, hidden_size)
-        self.i2o_poses = nn.Linear(input_size + hidden_size, output_size)
+
+        self.i2o_poses = nn.Linear(input_size + hidden_size, hidden_size)
+
+        self.fc1_poses = nn.Linear(hidden_size, hidden_size)
+        self.fc2_poses = nn.Linear(hidden_size, hidden_size)
+        self.fc3_poses = nn.Linear(hidden_size, hidden_size)
+        self.fc4_poses = nn.Linear(hidden_size, output_size)
+
         self.softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, input, hidden):
@@ -17,7 +24,14 @@ class PInSoRoRNN(nn.Module):
 
         combined_poses = torch.cat((input, hidden), 1)
         hidden_poses = self.i2h_poses(combined_poses)
-        output_poses = self.i2o_poses(combined_poses)
+        output_poses0 = self.i2o_poses(combined_poses)
+
+        output_poses1 = self.fc1_poses(output_poses0)
+        output_poses2 = self.fc2_poses(output_poses1)
+        output_poses3 = self.fc3_poses(output_poses2)
+
+        output_poses = self.fc4_poses(output_poses3)
+
         output = self.softmax(output_poses)
         return output, hidden_poses
 
